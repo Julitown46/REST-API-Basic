e4839473f88ff68209d8a56a441380510a32dd02
Julitown46 <julian.moreno@ebury.com>
2025-04-24 09:55:45 +0200

    New model test (wrong)

--- projects/models.py
+++ projects/models.py
@@ -5,4 +5,5 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     """members = models.IntegerField(default=0)"""
+    """Lo de arriba se puede borrar"""
     created_at = models.DateTimeField(auto_now_add=True)
