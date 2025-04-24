515ff74fb47f5e6e70b3d070c6a039e44eaa784b
Julitown46 <julian.moreno@ebury.com>
2025-04-24 09:54:02 +0200

    New model test

--- projects/models.py
+++ projects/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    """members = models.IntegerField(default=0)"""
     created_at = models.DateTimeField(auto_now_add=True)
